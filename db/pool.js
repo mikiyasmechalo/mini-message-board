e473fec6356a65bd6aff3947c4ec89ef1f8731aa
mikiyasmechalo <mikiyasmechalo70@gmail.com>
2025-08-28 19:57:51 +0300

    debugging "getaddrinfo ENOTFOUND base"

--- db/pool.js
+++ db/pool.js
@@ -2,6 +2,16 @@ import { Pool } from "pg";
 import dotenv from "dotenv";
 dotenv.config();
 
+const dbUrl = process.env.DB_URL || "NOT SET";
+
+if (dbUrl !== "NOT SET") {
+  const masked = dbUrl.replace(/:\/\/.*@/, "://****:****@");
+  console.log("DB_URL (masked):", masked);
+} else {
+  console.log("DB_URL is NOT set!");
+}
+
+
 export default new Pool({
   connectionString: process.env.DB_URL,
   ssl: {
